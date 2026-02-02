bdd30d26ad58a9158e024b215dc9b26baa5b1263
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2026-02-02 22:55:45 +0100

    Implemented initialization logic of StorageActor

--- src/storage_manager.rs
+++ src/storage_manager.rs
@@ -1,38 +1,84 @@
+use std::fs::{create_dir, exists, read_dir};
+use esp_idf_hal::gpio;
+use esp_idf_hal::peripheral::Peripheral;
+use esp_idf_hal::sd::mmc::{SdMmcHostConfiguration, SdMmcHostDriver, SDMMC1};
+use esp_idf_hal::sd::{SdCardConfiguration, SdCardDriver};
+use esp_idf_svc::fs::fatfs::Fatfs;
+use esp_idf_svc::io::vfs::MountedFatfs;
+
+const PAYLOADS_DIR: &str = "/data/payloads";
+
+pub struct StorageActor {
+    fs: MountedFatfs<Fatfs<SdCardDriver<SdMmcHostDriver<'static>>>>,
+}
 pub struct File {
     name: String,
     size: u8,
     location: String,
 }
 
-pub fn read_files<'a>() -> anyhow::Result<Vec<File>> {
-    let mut files = vec![];
+impl StorageActor {
+    pub fn new(
+        mmc: impl Peripheral<P = SDMMC1> + 'static,
+        cmd: impl Peripheral<P = gpio::Gpio15> + 'static,
+        clk: impl Peripheral<P = gpio::Gpio14> + 'static,
+        d0: impl Peripheral<P = gpio::Gpio2> + 'static,
+        d1: impl Peripheral<P = gpio::Gpio4> + 'static,
+        d2: impl Peripheral<P = gpio::Gpio12> + 'static,
+        d3: impl Peripheral<P = gpio::Gpio13> + 'static,
+    ) -> anyhow::Result<Self> {
+        let sd_card_driver = SdCardDriver::new_mmc(
+            SdMmcHostDriver::new_4bits(
+                mmc,
+                cmd,
+                clk,
+                d0,
+                d1,
+                d2,
+                d3,
+                None::<gpio::AnyIOPin>,
+                None::<gpio::AnyIOPin>,
+                &SdMmcHostConfiguration::new(),
+            )?,
+            &SdCardConfiguration::new(),
+        )?;
 
-    // TODO: Implement actual storage reading from SD Card
-    files.push(File {
-        name: "hello_world".to_string(),
-        size: 4,
-        location: "/scripts/hello_world.ox".to_string(),
-    });
-    files.push(File {
-        name: "basic_script".to_string(),
-        size: 3,
-        location: "/scripts/basic_script.ox".to_string(),
-    });
-    files.push(File {
-        name: "advanced_script".to_string(),
-        size: 5,
-        location: "/scripts/advanced_script.ox".to_string(),
-    });
-    files.push(File {
-        name: "find_files".to_string(),
-        size: 2,
-        location: "/scripts/find_files.ox".to_string(),
-    });
+        let mounted_fatfs = MountedFatfs::mount(Fatfs::new_sdcard(0, sd_card_driver)?, "/data", 4)?;
 
-    Ok(files)
-}
+        if !exists(&PAYLOADS_DIR)? {
+            create_dir(&PAYLOADS_DIR)?;
+        };
 
-pub fn get_file_content(file: String) -> anyhow::Result<String> {
-    // TODO: Implement actual file reading from SD Card
-    Ok(String::from("KEY GUI"))
+        Ok(Self { fs: mounted_fatfs })
+    }
+
+    pub fn read_files<'a>() -> anyhow::Result<Vec<File>> {
+        let mut files = vec![];
+
+        if let Ok(directory_content) = read_dir(&PAYLOADS_DIR) {
+            let entries: Vec<_> = directory_content.collect();
+            if entries.len() > 0 {
+                for file_result in entries {
+                    let file = file_result?;
+                    let f = File{
+                        name: String::from(
+                            file.file_name().to_str().unwrap()
+                        ),
+                        size: (file.metadata()?.len() / 1024) as u8,
+                        location: String::from(
+                            file.path().to_str().unwrap()
+                        ),
+                    };
+                    files.push(f);
+                }
+            }
+        }
+
+        Ok(files)
+    }
+
+    pub fn get_file_content(file: String) -> anyhow::Result<String> {
+        // TODO: Implement actual file reading from SD Card
+        Ok(String::from("KEY GUI"))
+    }
 }
