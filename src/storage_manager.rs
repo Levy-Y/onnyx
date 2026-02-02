27483dbc1590bb7a3880528d237edac1a59518b5
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2026-02-02 20:31:19 +0100

    Added an additional placeholder function to storage_manager

--- src/storage_manager.rs
+++ src/storage_manager.rs
@@ -1,33 +1,38 @@
-pub struct File<'a> {
-    name: &'a str,
+pub struct File {
+    name: String,
     size: u8,
-    location: &'a str,
+    location: String,
 }
 
-pub fn read_files<'a>() -> anyhow::Result<Vec<File<'a>>> {
+pub fn read_files<'a>() -> anyhow::Result<Vec<File>> {
     let mut files = vec![];
 
-    // TODO: Implement actual storage file reading from SD Card
+    // TODO: Implement actual storage reading from SD Card
     files.push(File {
-        name: "hello_world",
+        name: "hello_world".to_string(),
         size: 4,
-        location: "/scripts/hello_world.ox",
+        location: "/scripts/hello_world.ox".to_string(),
     });
     files.push(File {
-        name: "basic_script",
+        name: "basic_script".to_string(),
         size: 3,
-        location: "/scripts/basic_script.ox",
+        location: "/scripts/basic_script.ox".to_string(),
     });
     files.push(File {
-        name: "advanced_script",
+        name: "advanced_script".to_string(),
         size: 5,
-        location: "/scripts/advanced_script.ox",
+        location: "/scripts/advanced_script.ox".to_string(),
     });
     files.push(File {
-        name: "find_files",
+        name: "find_files".to_string(),
         size: 2,
-        location: "/scripts/find_files.ox",
+        location: "/scripts/find_files.ox".to_string(),
     });
 
     Ok(files)
+}
+
+pub fn get_file_content(file: String) -> anyhow::Result<String> {
+    // TODO: Implement actual file reading from SD Card
+    Ok(String::from("KEY GUI"))
 }
