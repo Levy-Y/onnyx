c0c98f537cca60607e73e8226e53fc2254d473ff
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2026-02-03 10:08:44 +0100

    Refactored executor to use the new get_file_content() from the StorageManager

--- src/executor.rs
+++ src/executor.rs
@@ -4,18 +4,13 @@ mod errors {
     pub mod executor_errors;
 }
 
+pub use crate::executor::errors::executor_errors::ExecutorError;
+pub use enums::Actions;
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 use std::time::Duration;
-pub use crate::executor::errors::executor_errors::ExecutorError;
-pub use enums::Actions;
-use crate::storage_manager::get_file_content;
-// use std::fs;
-
-// pub fn read_script_file(path: &str) -> String {
-//     fs::read_to_string(path).unwrap()
-// }
+use crate::storage_manager::STORAGE;
 
 pub struct ExecutorActor;
 
@@ -35,22 +30,22 @@ impl ExecutorActor {
     }
 
     fn handle_action(script_name: String) -> anyhow::Result<()> {
-        let script_content = get_file_content(script_name)?;
+        let script_content = STORAGE.get().unwrap().get_file_content(script_name)?;
         let parsed_actions = Self::parse_script(&script_content)?;
         Self::execute_actions(parsed_actions);
 
         Ok(())
     }
-    
+
     fn parse_script(script_content: &String) -> Result<Vec<Actions>, ExecutorError> {
         if script_content.len() == 0 {
             return Err(ExecutorError::TaskFailed(String::from(
                 "Cannot parse an empty file.",
             )));
         }
-    
+
         let mut actions: Vec<Actions> = vec![];
-    
+
         for line in script_content.lines() {
             let action = Actions::from_line(line);
             if action.is_err() {
@@ -58,13 +53,13 @@ impl ExecutorActor {
                     "Error while parsing script.".to_string(),
                 ));
             }
-    
+
             actions.push(action.unwrap());
         }
-    
+
         Ok(actions)
     }
-    
+
     fn execute_actions(actions: Vec<Actions>) {
         for action in &actions {
             match action {
