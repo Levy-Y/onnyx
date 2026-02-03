ffa851486543c05ea7633404ef601aca61cd6aed
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2026-02-03 10:16:21 +0100

    Added StorageManager initialization logic to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![feature(mpmc_channel)]
-
 mod display_manager;
 mod executor;
 mod led_manager;
@@ -12,14 +10,14 @@ use std::thread;
 use std::time::Duration;
 
 use crate::display_manager::{init_display, init_ui, set_state};
+use crate::executor::ExecutorActor;
+use crate::storage_manager::{File, StorageManager};
 use crate::web_server::WebActor;
 use crate::wifi_manager::{await_network_start, get_ap_info, init_ap_modem, PasswordString};
 use display_manager::DeviceState;
 use esp_idf_hal::{delay::FreeRtos, prelude::Peripherals};
 use esp_idf_svc::{log::EspLogger, sys::link_patches};
 use log::info;
-use crate::executor::ExecutorActor;
-use crate::storage_manager::File;
 
 fn main() -> anyhow::Result<()> {
     link_patches();
@@ -57,11 +55,18 @@ fn main() -> anyhow::Result<()> {
 
     let tx_clone1 = web_executor_tx.clone();
 
-    storage_web_tx.send(vec![])?;
+    StorageManager::init(
+        peripherals.sdmmc1,
+        peripherals.pins.gpio16,
+        peripherals.pins.gpio12,
+        peripherals.pins.gpio14,
+        peripherals.pins.gpio17,
+        peripherals.pins.gpio21,
+        peripherals.pins.gpio18,
+        storage_web_tx
+    )?;
 
-    thread::spawn(move || {
-        ExecutorActor::start(web_executor_rx)
-    });
+    thread::spawn(move || ExecutorActor::start(web_executor_rx));
 
     thread::Builder::new()
         .stack_size(8192)
